63b6b4ae6ec6a26ac04248a75647600397c7fe41
calumgould <calumgould26@gmail.com>
2020-06-04 01:33:09 +0100

    added firebase login auth

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions";
 
-const Account = () => {
+const Account = (props) => {
+
+    const handleLogout = () => {
+        const { dispatch } = props;
+        dispatch(logoutUser());
+    };
+
+    const { isLoggingOut, logoutError } = props;
+
     return ( 
-        <>
-            <h2>Account</h2>
-        </>
+        <div>
+            <h2>This is your app's protected area.</h2>
+            <p>Any routes here will also be protected</p>
+            <button onClick={handleLogout}>Logout</button>
+            {isLoggingOut && <p>Logging Out....</p>}
+            {logoutError && <p>Error logging out</p>}
+      </div>
      );
 }
  
-export default Account;
+function mapStateToProps(state) {
+    return {
+      isLoggingOut: state.auth.isLoggingOut,
+      logoutError: state.auth.logoutError
+    };
+  }
+
+export default connect(mapStateToProps)(Account);
