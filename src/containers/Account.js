77d70439181d5ea16a89206ab250e19e905feb3a
calumgould <calumgould26@gmail.com>
2020-06-06 20:18:20 +0100

    styled account page

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import DisplayImage from 'components/DisplayImage';
 import { storage } from '../firebase/firebase';
 import { uploadUserProfileImage } from 'actions';
+
+import 'styles/Account.css';
 
 const Account = (props) => {
 
@@ -11,7 +12,15 @@ const Account = (props) => {
   const [image, setImage] = useState(null);
   const [imageURL, setImageURL] = useState(null);
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [showProgress, setShowProgress] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
+
+  useEffect(() => {
+    if(uploadProgress === 100){
+      setShowEdit(false);
+      setUploadProgress(0);
+    }
+  },[uploadProgress])
 
   const handleDisplayImage = (event) => {
     let reader = new FileReader();
@@ -25,6 +34,7 @@ const Account = (props) => {
 
   const handleImageUpload = (event) => {
     if (image !== null) {
+      setShowProgress(true)
       const uploadTask = storage.ref(`profiles/${user.uid}/${image.name}`).put(image)
       uploadTask.on(
         'state_changed',
@@ -48,35 +58,64 @@ const Account = (props) => {
             });
         }
       );
-      setShowEdit(false);
     } else {
       return 'No image uploaded';
     }
   }
 
+  const formatDate = (date) => {
+    return date.split(' ').slice(0, 4).join(' ');
+  }
+
   const EditProfileImage = () => (
-    <div>
-      <progress value={uploadProgress} max='100' />
-      <input type='file' name='profile-image' onChange={handleDisplayImage} />
-      <button className='button' onClick={handleImageUpload} >Upload Image</button>
+    <div className='edit-wrapper'>
+      {showProgress ? <progress value={uploadProgress} max='100' /> : null}
+      <div>
+        <label className='upload-button' for='upload'>
+          <input id='upload' type='file' name='profile-image' onChange={handleDisplayImage} />
+          Choose File
+        </label>
+      </div>
+      <div>
+        <button className='button' onClick={handleImageUpload} >Upload Image</button>
+      </div>
     </div>
   )
 
-  return (
+ 
+  if(user.displayName !== null) return (
     <div>
       <h2>Account</h2>
-      <div>
-        <h3>Your Details</h3>
-        <p>Display Name</p>
-        <p>{user.displayName}</p>
-        <p>Email</p>
-        <p>{user.email}</p>
-      </div>
+      <div className='account-page'>
+        <div className='details-wrapper'>
+        
+          <div className='details-text'>
+            <h3>Your Details</h3>
+            <h4>Display Name</h4>
+            <p>{user.displayName}</p>
+            <h4>Email</h4>
+            <p>{user.email}</p>
+            <h4>Date Created</h4>
+            <p>{formatDate(user.metadata.creationTime)}</p>
+            <h4>Last Login</h4>
+            <p>{formatDate(user.metadata.lastSignInTime)}</p>
+            <button className='button'>Edit Details</button>
 
-      <div>
-        <h3>Profile Image</h3>
-        <DisplayImage imageURL={imageURL || user.photoURL} />
-        {showEdit ? <EditProfileImage /> : <button className='button' onClick={() => setShowEdit(true)}>Change Image</button>}
+          </div>
+
+        </div>
+  
+        <div className='profile-wrapper'>
+          <div className='image-wrapper'>
+            <h3>Profile Image</h3>
+            <img className='profile-image' src={imageURL || user.photoURL} alt='uploaded'/>
+          </div>
+            {showEdit ? <EditProfileImage /> : 
+            <div className='edit-wrapper'>
+              <button className='button' onClick={() => setShowEdit(true)}>Change Image</button>
+            </div>
+            }
+        </div>
       </div>
     </div>
   );
