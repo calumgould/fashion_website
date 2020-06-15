36e831609f1ecb58b6550307102402c6f427c30b
calumgould <calumgould26@gmail.com>
2020-06-15 23:52:50 +0100

    added table to cart page

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { connect } from "react-redux";
 
-const Cart = () => {
+
+const Cart = ({cart}) => {
+
+    const generateCartItems = cart.map(cartItem => {
+        return (
+            <tr key={cartItem.item_id}>
+                <td>
+                    <img src={cartItem.item_img} alt={cartItem.item_name} />
+                </td>
+                <td>{cartItem.item_name}</td>
+                <td>{cartItem.item_desc}</td>
+                <td>{cartItem.quantity}</td>
+                <td>${cartItem.price}</td>
+            </tr>
+        )
+    })
+
     return ( 
         <div>
             <h2>Cart</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {generateCartItems}
+                </tbody>
+            </table>
         </div>
      );
 }
  
-export default Cart;
+function mapStateToProps(state) {
+    return {
+        cart: state.userActions.cart
+    };
+  }
+  
+  export default (connect(mapStateToProps)(Cart));
